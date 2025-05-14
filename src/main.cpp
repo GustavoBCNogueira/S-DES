10648a038bd435620dec7067d17f05ee607cd117
fornari03 <221017023@aluno.unb.br>
2025-05-14 16:46:48 -0300

    feat: faz uma main para o usuário escolher qual operação realizar

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include "encrypt_sdes.h"
 
 int main() {
-    bitset<8> block("10101011"); // texto claro em binário de 8 bits
-    bitset<10> key("1010000010"); // chave de 10 bits
-    tuple<bitset<8>, bitset<8>> keys = generate_keys(key);
-    bitset<8> key1 = get<0>(keys);
-    bitset<8> key2 = get<1>(keys);
+    int option;
+    cout << "\n\nEscolha uma opção:" << endl;
+    cout << "1. Encriptar com S-DES" << endl;
+    cout << "2. Decriptar com S-DES" << endl;
+    cout << "3. Encriptar com ECB-SDES" << endl;
+    cout << "4. Encriptar com CBC-SDES" << endl;
+    cout << "Qualquer outro caractere irá sair do programa" << endl;
+    cin >> option;
 
-    cout << "Input Block: " << block << endl;
-    cout << "Key1: " << key1 << endl;
-    cout << "Key2: " << key2 << endl;
+    if (option == 1) {
+        bitset<8> block("10101011"); // texto claro em binário de 8 bits
+        bitset<10> key("1010000010"); // chave de 10 bits
+    
+        bitset<8> encrypted_block = encrypt_sdes(block, key);
+    
+        cout << "Encrypted Block: " << encrypted_block << endl;
 
-    bitset<8> encrypted_block = encryption(block, key1, key2);
+        main();
+    }
 
-    cout << "Encrypted Block: " << encrypted_block << endl;
+    else if (option == 2) {}
+    else if (option == 3) {}
+    else if (option == 4) {}
 
     return 0;
 }
