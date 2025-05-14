72d18cda30feed0100c10b8de50e816fd91b8fef
fornari03 <221017023@aluno.unb.br>
2025-05-14 20:12:49 -0300

    fix: parâmetro default do printState

--- src/utils.cpp
+++ src/utils.cpp
@@ -10,14 +10,14 @@ char bitsetToChar(const bitset<8>& bits) {
     return static_cast<char>(bits.to_ulong());
 }
 
-void printState(const bitset<4>& L, const bitset<4>& R, string state_name="") {
+void printState(const bitset<4>& L, const bitset<4>& R, string state_name) {
     if (!state_name.empty())
         cout << "Estado após " << state_name << ":" << endl;
     cout << "L: " << L << endl;
     cout << "R: " << R << "\n\n";
 }
 
-void printState(const bitset<8>&block, string state_name="") {
+void printState(const bitset<8>&block, string state_name) {
     if (!state_name.empty())
         cout << "Estado após " << state_name << ":" << endl;
     cout << "Bloco: " << "\n\n";
