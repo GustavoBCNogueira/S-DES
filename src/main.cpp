9a6c00882892c10c722b7a74a60c932b3f46a8b1
fornari03 <221017023@aluno.unb.br>
2025-05-14 17:11:11 -0300

    feat: implementa a encriptação no modo ECB

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,35 @@ int main() {
 
         main();
     }
-    else if (option == 3) {}
+
+    else if (option == 3) {
+        string input_bits = "101010111010101110101011"; // texto claro binário de 24 bits (3 blocos)
+    
+        bitset<10> key("1010000010"); // chave de 10 bits
+        vector<bitset<8>> encrypted_blocks;
+    
+        // número de blocos de 8 bits
+        int num_blocks = input_bits.size() / 8;
+    
+        for (int i = 0; i < num_blocks; i++) {
+            string block_str = input_bits.substr(i * 8, 8);
+            bitset<8> current_block(block_str);
+    
+            bitset<8> encrypted_block = encrypt_sdes(current_block, key);
+            encrypted_blocks.push_back(encrypted_block);
+        }
+    
+        // mostra os blocos cifrados
+        cout << "Blocos cifrados em ECB:" << endl;
+        for (const auto& block : encrypted_blocks) {
+            cout << block << " ";
+        }
+        cout << endl;
+    
+        main();
+    }
+    
+
     else if (option == 4) {}
 
     return 0;
