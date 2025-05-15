60888f65b682dad7b5663ef189be30110f70336b
fornari03 <221017023@aluno.unb.br>
2025-05-15 16:13:08 -0300

    feat: implementa o modo CBC e faz a main rodar só uma vez por execução

--- src/main.cpp
+++ src/main.cpp
@@ -38,8 +38,6 @@ int main() {
             
             cout << "Bloco cifrado: " << encrypted_block << endl;
         }
-        
-        main();
     }
 
     else if (option == 2) {
@@ -53,7 +51,6 @@ int main() {
 
         cout << "Bloco decriptado: " << block << endl;
         // 10101000
-        main();
     }
 
     else if (option == 3) {
@@ -75,16 +72,60 @@ int main() {
     
         // mostra os blocos cifrados
         cout << "Blocos cifrados em ECB:" << endl;
+        cout << "Em binário: ";
         for (const auto& block : encrypted_blocks) {
             cout << block << " ";
         }
         cout << endl;
-    
-        main();
+        cout << "Em hexadecimal: 0x";
+        for (const auto& block : encrypted_blocks) {
+            cout << hex << uppercase << block.to_ulong();
+        }
+        cout << endl;
     }
     
 
-    else if (option == 4) {}
+    else if (option == 4) {
+        cout << "Digite o texto a ser cifrado em CBC-SDES: ";
+        string text;
+        cin >> text;
+
+        bitset<10> key("1010000010"); // chave de 10 bits
+        vector<bitset<8>> encrypted_blocks;
+
+        // número de blocos de 8 bits
+        int num_blocks = text.size();
+
+        // vetor de inicialização (IV)
+        bitset<8> iv("01010101"); // FIXADO pela especificação do trabalho
+
+        for (int i = 0; i < num_blocks; i++) {
+            bitset<8> current_block = charToBitset(text[i]);
+            if (i == 0) {
+                // pro primeiro bloco, XOR com o vetor de inicialização
+                current_block ^= iv;
+            }
+            else {
+                // pro resto dos blocos, XOR com o bloco cifrado anterior
+                current_block ^= encrypted_blocks[i - 1];
+            }
+            bitset<8> encrypted_block = encrypt_sdes(current_block, key);
+            encrypted_blocks.push_back(encrypted_block);
+        }
+
+        // mostra os blocos cifrados
+        cout << "Blocos cifrados em CBC:" << endl;
+        cout << "Em binário: ";
+        for (const auto& block : encrypted_blocks) {
+            cout << block << " ";
+        }
+        cout << endl;
+        cout << "Em hexadecimal: 0x";
+        for (const auto& block : encrypted_blocks) {
+            cout << hex << uppercase << block.to_ulong();
+        }
+        cout << endl;
+    }
 
     return 0;
 }
