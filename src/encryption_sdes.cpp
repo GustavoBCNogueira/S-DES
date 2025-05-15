039963e0938fd0c632a42c7619c1863647277168
fornari03 <221017023@aluno.unb.br>
2025-05-15 18:27:21 -0300

    fix: sbox_lookup, encrypt_sdes e decrypt_sdes

--- src/encryption_sdes.cpp
+++ src/encryption_sdes.cpp
@@ -5,31 +5,60 @@
 using namespace std;
 
 
-bitset<4> sbox_lookup(bitset<8> &xor_result) {
-
+bitset<4> sbox_lookup(bitset<8>& input) {
+    // s-box 0
     int s0[4][4] = {
         {1, 0, 3, 2},
         {3, 2, 1, 0},
         {0, 2, 1, 3},
         {3, 1, 3, 2}
     };
-    
+
+    // s-box 1
     int s1[4][4] = {
-        {0, 1, 2, 3},   
+        {0, 1, 2, 3},
         {2, 0, 1, 3},
         {3, 0, 1, 0},
         {2, 1, 0, 3}
     };
+    // s-boxes são definidas pelo algoritmo (FIXAS)
 
-    int row1_x = (xor_result[7] << 1) | xor_result[4];
-    int row1_y = (xor_result[5] << 1) | xor_result[6];
-    int row2_x = (xor_result[3] << 1) | xor_result[0];
-    int row2_y = (xor_result[1] << 1) | xor_result[2];
-    int s0_value = s0[row1_x][row1_y];
-    int s1_value = s1[row2_x][row2_y];
+    // pega os bits para s0
 
-    return bitset<4>((bitset<2>(s0_value).to_ulong() << 2) | bitset<2>(s1_value).to_ulong());
+    int s0_row_bit1 = input[7];
+    int s0_row_bit0 = input[4];
+    // junta os dois bits para formar a linha
+    int s0_row = (s0_row_bit1 << 1) | s0_row_bit0;
+
+    int s0_col_bit1 = input[6];
+    int s0_col_bit0 = input[5];
+    // junta os dois bits para formar a coluna
+    int s0_col = (s0_col_bit1 << 1) | s0_col_bit0;
+
+
+    // pega os bits para s1
+
+    int s1_row_bit1 = input[3];
+    int s1_row_bit0 = input[0];
+    // junta os dois bits para formar a linha
+    int s1_row = (s1_row_bit1 << 1) | s1_row_bit0;
+
+    int s1_col_bit1 = input[2];
+    int s1_col_bit0 = input[1];
+    // junta os dois bits para formar a coluna
+    int s1_col = (s1_col_bit1 << 1) | s1_col_bit0;
+
+    // aplica as S-boxes
+    int s0_value = s0[s0_row][s0_col];
+    int s1_value = s1[s1_row][s1_col];
+
+    // junta os dois valores em um único de 4 bits
+    int value = (s0_value << 2) | s1_value;
+
+    bitset<4> result(value);
+    return result;
 }
+
 
 bitset<4> fk(bitset<4> &half, bitset<8> &key) {
     cout << "Função FK:" << endl;
@@ -76,8 +105,8 @@ bitset<8> encrypt_sdes(bitset<8> &block, bitset<10> &key) {
     // PRIMEIRA RODADA
     bitset<4> fk_output = fk(R, key1);
     
-    R = L ^ fk_output; // faz o XOR entre L e o resultado da função fk
-    printState(L, R, "FK");
+    L = L ^ fk_output; // faz o XOR entre L e o resultado da função fk
+    printState(L, R, "Feistel");
 
     // permuta as duas metades
     swap(L, R);
@@ -88,8 +117,8 @@ bitset<8> encrypt_sdes(bitset<8> &block, bitset<10> &key) {
     fk_output = fk(R, key2);
     
     // faz o XOR entre L e o resultado da função fk
-    R = L ^ fk_output;
-    printState(L, R, "FK");
+    L = L ^ fk_output;
+    printState(L, R, "Feistel");
 
     // junta L e R
     bitset<8> combined_block = bitset<8>((L.to_ulong() << 4) | R.to_ulong());
@@ -121,32 +150,32 @@ bitset<8> decrypt_sdes(bitset<8> &cipher_block, bitset<10> &key) {
 
     printState(L, R);
 
+
     // PRIMEIRA RODADA
     bitset<4> fk_output = fk(R, key2);
-    printState(L, R, "FK");
-
-    R = L ^ fk_output; // faz o XOR entre L e o resultado da função fk
-    printState(L, R, "XOR de L com FK");
+    
+    L = L ^ fk_output; // faz o XOR entre L e o resultado da função fk
+    printState(L, R, "Feistel");
 
     // permuta as duas metades
     swap(L, R);
-    printState(L, R, "SWITCH");
+    printState(L, R, "SWITCH"),
 
 
     // SEGUNDA RODADA
     fk_output = fk(R, key1);
-    printState(L, R, "FK");
-
+    
     // faz o XOR entre L e o resultado da função fk
-    R = L ^ fk_output;
-    printState(L, R, "XOR de L com FK");
+    L = L ^ fk_output;
+    printState(L, R, "Feistel");
 
     // junta L e R
     bitset<8> combined_block = bitset<8>((L.to_ulong() << 4) | R.to_ulong());
-    printState(L, R, "juntar L e R");
+    printState(cipher_block, "juntar L e R");
 
     // faz o IP^-1
-    bitset<8> block = inv_IP(combined_block);
+    cipher_block = inv_IP(combined_block);
+    printState(cipher_block, "IP^-1");
 
-    return block;
+    return cipher_block;
 }
