806dfec692b1e27cd86bf8caeb30c30b49d8e211
GustavoBCNogueira <nogueiragustavo207@gmail.com>
2025-05-16 19:27:28 -0300

    Added operation modes decryption

--- headers/op_modes.h
+++ headers/op_modes.h
@@ -6,7 +6,11 @@ using namespace std;
 
 vector<bitset<8>> encrypt_ecb_sdes(string text, bitset<10> key);
 vector<bitset<8>> encrypt_ecb_sdes(vector<bitset<8>> blocks, bitset<10> key);
+vector<bitset<8>> decrypt_ecb_sdes(string text, bitset<10> key);
+vector<bitset<8>> decrypt_ecb_sdes(vector<bitset<8>> blocks, bitset<10> key);
 vector<bitset<8>> encrypt_cbc_sdes(string text, bitset<10> key);
 vector<bitset<8>> encrypt_cbc_sdes(vector<bitset<8>> blocks, bitset<10> key);
+vector<bitset<8>> decrypt_cbc_sdes(string text, bitset<10> key);
+vector<bitset<8>> decrypt_cbc_sdes(vector<bitset<8>> blocks, bitset<10> key);
 
 #endif
--- src/op_modes.cpp
+++ src/op_modes.cpp
@@ -31,6 +31,33 @@ vector<bitset<8>> encrypt_ecb_sdes(vector<bitset<8>> blocks, bitset<10> key) {
     return encrypted_blocks;
 }
 
+vector<bitset<8>> decrypt_ecb_sdes(string text, bitset<10> key) {
+    vector<bitset<8>> decrypted_blocks;
+
+    // número de blocos de 8 bits
+    int num_blocks = text.size();
+
+    for (int i = 0; i < num_blocks; i++) {
+        bitset<8> current_block = charToBitset(text[i]);
+        bitset<8> decrypted_block = decrypt_sdes(current_block, key);
+        decrypted_blocks.push_back(decrypted_block);
+    }
+    return decrypted_blocks;
+}
+
+vector<bitset<8>> decrypt_ecb_sdes(vector<bitset<8>> blocks, bitset<10> key) {
+    vector<bitset<8>> decrypted_blocks;
+
+    // número de blocos de 8 bits
+    int num_blocks = blocks.size();
+
+    for (int i = 0; i < num_blocks; i++) {
+        bitset<8> decrypted_block = decrypt_sdes(blocks[i], key);
+        decrypted_blocks.push_back(decrypted_block);
+    }
+    return decrypted_blocks;
+}
+
 
 vector<bitset<8>> encrypt_cbc_sdes(string text, bitset<10> key) {
     vector<bitset<8>> encrypted_blocks;
@@ -80,4 +107,53 @@ vector<bitset<8>> encrypt_cbc_sdes(vector<bitset<8>> blocks, bitset<10> key) {
         encrypted_blocks.push_back(encrypted_block);
     }
     return encrypted_blocks;
+}
+
+vector<bitset<8>> decrypt_cbc_sdes(string text, bitset<10> key) {
+    vector<bitset<8>> decrypted_blocks;
+
+    // número de blocos de 8 bits
+    int num_blocks = text.size();
+
+    // vetor de inicialização (IV)
+    bitset<8> iv("01010101"); // FIXADO pela especificação do trabalho
+
+    for (int i = num_blocks-1; i >= 0; i--) {
+        bitset<8> current_block = charToBitset(text[i]);
+        bitset<8> decrypted_block = decrypt_sdes(current_block, key);
+        if (i == 0) {
+            // pro primeiro bloco, XOR com o vetor de inicialização
+            decrypted_block ^= iv;
+        }
+        else {
+            // pro resto dos blocos, XOR com o bloco cifrado anterior
+            decrypted_block ^= text[i - 1];
+        }
+        decrypted_blocks.insert(decrypted_blocks.begin(), decrypted_block);
+    }
+    return decrypted_blocks;
+}
+
+vector<bitset<8>> decrypt_cbc_sdes(vector<bitset<8>> blocks, bitset<10> key) {
+    vector<bitset<8>> decrypted_blocks;
+
+    // número de blocos de 8 bits
+    int num_blocks = blocks.size();
+
+    // vetor de inicialização (IV)
+    bitset<8> iv("01010101"); // FIXADO pela especificação do trabalho
+
+    for (int i = num_blocks-1; i >= 0; i--) {
+        bitset<8> decrypted_block = decrypt_sdes(blocks[i], key);
+        if (i == 0) {
+            // pro primeiro bloco, XOR com o vetor de inicialização
+            decrypted_block ^= iv;
+        }
+        else {
+            // pro resto dos blocos, XOR com o bloco cifrado anterior
+            decrypted_block ^= blocks[i - 1];
+        }
+        decrypted_blocks.insert(decrypted_blocks.begin(), decrypted_block);
+    }
+    return decrypted_blocks;
 }
